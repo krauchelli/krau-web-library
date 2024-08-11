654d1ccc42f2b7e8d93f7a473d30448a1f293ed0
Krauchelli <mekaku26shi@gmail.com>
2024-08-12 00:48:02 +0700

    refactor: Update create book view with Bootstrap layout

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -1,42 +1,37 @@
-<!DOCTYPE html>
-<html>
+@extends('layouts.app')
 
-<head>
-    <title>Create Book</title>
-</head>
+@section('title', 'Create new Book')
 
-<body>
-    <div class="container">
-        <h1>Create New Book</h1>
-        <form action="{{ route('books.store') }}" method="POST">
-            @csrf
-            <div class="form-group">
-                <label for="title">Title</label>
-                <input type="text" name="title" class="form-control" required>
-            </div>
-            <div class="form-group">
-                <label for="author">Author</label>
-                <input type="text" name="author" class="form-control" required>
-            </div>
-            <div class="form-group">
-                <label for="published_date">Published Date</label>
-                <input type="date" name="published_date" class="form-control" required>
-            </div>
-            <div class="form-group">
-                <label for="isbn">ISBN</label>
-                <input type="text" name="isbn" class="form-control" required>
-            </div>
-            <div class="form-group">
-                <label for="category_id">Category</label>
-                <select name="category_id" class="form-control" required>
-                    @foreach($categories as $category)
-                    <option value="{{ $category->id }}">{{ $category->name }}</option>
-                    @endforeach
-                </select>
-            </div>
-            <button type="submit" class="btn btn-primary">Create</button>
-        </form>
-    </div>
-</body>
-
-</html>
+@section('content')
+<div class="container">
+    <h1>Create New Book</h1>
+    <form action="{{ route('books.store') }}" method="POST">
+        @csrf
+        <div class="form-group">
+            <label for="title">Title</label>
+            <input type="text" name="title" class="form-control" required>
+        </div>
+        <div class="form-group">
+            <label for="author">Author</label>
+            <input type="text" name="author" class="form-control" required>
+        </div>
+        <div class="form-group">
+            <label for="published_date">Published Date</label>
+            <input type="date" name="published_date" class="form-control" required>
+        </div>
+        <div class="form-group">
+            <label for="isbn">ISBN</label>
+            <input type="text" name="isbn" class="form-control" required>
+        </div>
+        <div class="form-group">
+            <label for="category_id">Category</label>
+            <select name="category_id" class="form-control" required>
+                @foreach($categories as $category)
+                <option value="{{ $category->id }}">{{ $category->name }}</option>
+                @endforeach
+            </select>
+        </div>
+        <button type="submit" class="btn btn-primary">Create</button>
+    </form>
+</div>
+@endsection
