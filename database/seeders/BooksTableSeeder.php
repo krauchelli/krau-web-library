a4a781a678fe32d436ace881d61b8fcd64287d18
Krauchelli <mekaku26shi@gmail.com>
2024-08-11 11:54:02 +0700

    feat: Add seeders for books and categories tables

--- database/seeders/BooksTableSeeder.php
+++ database/seeders/BooksTableSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Carbon;
 
 class BooksTableSeeder extends Seeder
 {
@@ -13,6 +15,11 @@ class BooksTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('books')->insert([
+            ['title' => 'Sword Art Online', 'author' => 'Reki Kawahara', 'published_date' => '2009-04-10', 'isbn' => '9784048677608', 'category_id' => 1, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['title' => 'Toradora!', 'author' => 'Yuyuko Takemiya', 'published_date' => '2006-03-10', 'isbn' => '9784048677609', 'category_id' => 2, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['title' => 'No Game No Life', 'author' => 'Yuu Kamiya', 'published_date' => '2012-04-25', 'isbn' => '9784048677610', 'category_id' => 3, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['title' => 'Accel World', 'author' => 'Reki Kawahara', 'published_date' => '2009-02-10', 'isbn' => '9784048677611', 'category_id' => 4, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+        ]);
     }
 }
--- database/seeders/CategoriesTableSeeder.php
+++ database/seeders/CategoriesTableSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Carbon;
 
 class CategoriesTableSeeder extends Seeder
 {
@@ -13,6 +15,11 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('categories')->insert([
+            ['name' => 'Fantasy', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['name' => 'Romance', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['name' => 'Adventure', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+            ['name' => 'Science Fiction', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
+        ]);
     }
 }
