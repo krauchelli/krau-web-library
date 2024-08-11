125fe58043a4763a09699276f36ea298bc9aa083
Krauchelli <mekaku26shi@gmail.com>
2024-08-11 23:33:28 +0700

    feat: Update BooksExport class to include headings in exported Excel file

--- app/Exports/BooksExport.php
+++ app/Exports/BooksExport.php
@@ -4,14 +4,32 @@ namespace App\Exports;
 
 use App\Models\Book;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class BooksExport implements FromCollection
+class BooksExport implements FromCollection, WithHeadings
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
+    protected $books;
+
+    public function __construct($books)
+    {
+        $this->books = $books;
+    }
+
     public function collection()
     {
-        return Book::all();
+        return collect($this->books);
+    }
+
+    public function headings(): array
+    {
+        return [
+            'ID',
+            'Title',
+            'Author',
+            'Published Date',
+            'ISBN',
+            'Category ID',
+            // Add other relevant headings
+        ];
     }
 }
