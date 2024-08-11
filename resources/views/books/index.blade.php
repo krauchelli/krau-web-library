6127e16a2758d8cf0f5dfecb136a92919e1521a1
Krauchelli <mekaku26shi@gmail.com>
2024-08-12 01:00:35 +0700

    refactor: Update book index page with Bootstrap card layout

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -2,60 +2,32 @@
 
 @section('title', 'Books List')
 
+@section('header', 'Books List')
+
 @section('content')
 <div class="container">
-    <h1>Books List</h1>
-    <a href="{{ route('books.create') }}" class="btn btn-primary">Add New Book</a>
-    <a href="{{ route('books.export.pdf') }}" class="btn btn-primary">Export to PDF</a>
-    <a href="{{ route('books.export.excel') }}" class="btn btn-primary">Export to Excel</a>
-    <form action="{{ route('books.index') }}" method="GET" class="mt-3">
-        <div class="form-group">
-            <label for="category_id">Filter by Category</label>
-            <select name="category_id" class="form-control" onchange="this.form.submit()">
-                <option value="">All Categories</option>
-                @foreach($categories as $category)
-                <option value="{{ $category->id }}" {{ request('category_id') == $category->id ? 'selected' : '' }}>
-                    {{ $category->name }}</option>
-                @endforeach
-            </select>
-        </div>
-    </form>
-    <table class="table mt-3">
-        <thead>
-            <tr>
-                <th>Cover</th>
-                <th>Title</th>
-                <th>Author</th>
-                <th>Published Date</th>
-                <th>ISBN</th>
-                <th>Category</th>
-                <th>Actions</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($books as $book)
-            <tr>
-                <td>
-                    @if ($book->cover_image)
-                    <img src="{{ asset('storage/' . $book->cover_image) }}" alt="{{ $book->title }}" width="50">
-                    @else
-                    <img src="https://picsum.photos/50" alt="default image. Please upload!">
-                    @endif
-                </td>
-                <td>{{ $book->title }}</td>
-                <td>{{ $book->author }}</td>
-                <td>{{ $book->published_date }}</td>
-                <td>{{ $book->isbn }}</td>
-                <td>{{ $book->category->name }}</td>
-                <td>
+    <div class="row">
+        @foreach($books as $book)
+        <div class="col-md-4">
+            <div class="card mb-4">
+                @if ($book->cover_image)
+                <img src="{{ asset('storage/' . $book->cover_image) }}" class="card-img-top" alt="{{ $book->title }}">
+                @else
+                <img src="https://picsum.photos/200" class="card-img-top" alt="default image. Please upload!">
+                @endif
+                <div class="card-body">
+                    <h5 class="card-title">{{ $book->title }}</h5>
+                    <p class="card-text"><strong>Author:</strong> {{ $book->author }}</p>
+                    <p class="card-text"><strong>Published Date:</strong> {{ $book->published_date }}</p>
+                    <p class="card-text"><strong>ISBN:</strong> {{ $book->isbn }}</p>
+                    <p class="card-text"><strong>Category:</strong> {{ $book->category->name }}</p>
                     <a href="{{ route('books.show', $book->id) }}" class="btn btn-info">View</a>
                     <a href="{{ route('books.edit', $book->id) }}" class="btn btn-warning">Edit</a>
                     <a href="{{ route('books.uploadForm', $book->id) }}" class="btn btn-info">Upload Files</a>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-    </table>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
 </div>
 @endsection
